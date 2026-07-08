0ef4dd56beeea1f632504d75f093150b348216bf
chaibi <abdoochaib145@gmail.com>
2026-07-08 17:47:34 +0100

    v1

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
+  base: repositoryName ? `/${repositoryName}/` : '/',
   plugins: [react(), tailwindcss()],
 })
